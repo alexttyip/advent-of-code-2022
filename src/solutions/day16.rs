c514b38fa5bf6edf04eb2b0f95974aaf4cd7ddff
Alex Yip <tsztoyip@gmail.com>
2022-12-01 10:12:51 +0000

    Initial commit

---
+++ src/solutions/day16.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+type InputType = Vec<u32>;
+
+fn read_input() -> InputType {
+    fs::read_to_string("./inputs/day16.txt")
+        .unwrap()
+        .trim()
+        .lines()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn part1(_input: InputType) -> u32 {
+    0
+}
+
+fn part2(_input: InputType) -> u32 {
+    0
+}
+
+pub fn run() {
+    let input = read_input();
+
+    println!("--- Day 16 ---");
+    println!("Part 1: {}", part1(input.clone()));
+    println!("Part 2: {}", part2(input));
+}
