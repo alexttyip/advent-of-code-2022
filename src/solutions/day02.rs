26ae0b901dbd2a743c984d7ece896434a00bb095
Alex Yip <tsztoyip@gmail.com>
2022-12-02 10:43:05 +0000

    Day 2 done

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -1,28 +1,65 @@
 use std::fs;
 
-type InputType = Vec<u32>;
+use itertools::Itertools;
+
+type Int = u16;
+type Game = (Int, Int);
+type InputType = Vec<Game>;
+
+fn parse_char(s: &str) -> Int {
+    match s {
+        "A" | "X" => 0, // Rock / Lose
+        "B" | "Y" => 1, // Paper / Draw
+        "C" | "Z" => 2, // Scissors / Win
+        _ => panic!("Not a valid shape"),
+    }
+}
 
 fn read_input() -> InputType {
     fs::read_to_string("./inputs/day02.txt")
         .unwrap()
-        .trim()
         .lines()
-        .map(|s| s.parse().unwrap())
+        .map(|s| s.split_ascii_whitespace())
+        .map(|s| s.map(parse_char).collect_tuple().unwrap())
         .collect()
 }
 
-fn part1(_input: InputType) -> u32 {
-    0
+fn part1(input: InputType) -> Int {
+    input.iter().fold(0, |acc, (elf, me)| {
+        acc + if elf == &((me + 1) % 3) {
+            0
+        } else if me == elf {
+            1
+        } else {
+            2
+        } * 3 // Score for result
+            + (me + 1) // Score for shape
+    })
 }
 
-fn part2(_input: InputType) -> u32 {
-    0
+fn part2(input: InputType) -> Int {
+    input.iter().fold(0, |acc, (elf, result)| {
+        acc + *result * 3 // Score for result
+            + match *result {
+                0 => (elf + 3 - 1) % 3,
+                1 => *elf,
+                2 => (elf + 1) % 3,
+                _ => panic!(),
+            }
+            + 1 // Score for shape
+    })
 }
 
 pub fn run() {
     let input = read_input();
 
+    let part1 = part1(input.clone());
+    let part2 = part2(input);
+
+    assert_eq!(part1, 13268);
+    assert_eq!(part2, 15508);
+
     println!("--- Day 02 ---");
-    println!("Part 1: {}", part1(input.clone()));
-    println!("Part 2: {}", part2(input));
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
